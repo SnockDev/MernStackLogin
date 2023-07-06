9e35d39147fb70b399ef96371f9bab07c0c7a73a
Alejandro <groovysnock@gmail.com>
2023-07-06 14:12:52 -0400

    register and fixing model

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,7 +1,26 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
-export const register=async(req,res)=>{
-    res.send('registrando')
-}
-export const login=async(req,res)=>{
-    res.send('login')
-}
+export const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const passwordhashed = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      password: passwordhashed,
+    });
+
+    //await newUser.save();
+    res.status(201).json({"_id":newUser._id});
+    console.log(newUser);
+  } catch (error) {
+    res.status(400).send("error");
+    console.log(error);
+  }
+};
+export const login = async (req, res) => {
+  res.send("login");
+};
