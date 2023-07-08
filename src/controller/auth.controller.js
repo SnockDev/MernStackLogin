0dec6eedbe7ad8e1561e160106ece549b8a97274
Alejandro <groovysnock@gmail.com>
2023-07-08 10:21:50 -0400

    login and logout

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -19,13 +19,12 @@ export const register = async (req, res) => {
     const token = await signed({ id: userSaved._id });
 
     res.status(201).cookie("token", token).json({
-      _id:userSaved._id,
-      username:userSaved.username,
-      email:userSaved.email,
-      createdAt:userSaved.createdAt,
-      updatedAt:userSaved.updatedAt
+      _id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
     });
-
   } catch (err) {
     res.status(400).send("error");
     console.log(err);
@@ -33,5 +32,34 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("login");
+  try {
+    const { email, password } = req.body;
+
+    const userFound = await User.findOne({ email: email });
+    if (!userFound)
+      return res.status(400).json({ message: "user and/or pass incorrect" });
+    const ismatch = await bcrypt.compare(password, userFound.password);
+    if (!ismatch)
+      return res.status(400).json({ message: "user and/or pass incorrect" });
+
+    const token = await signed({ id: userFound._id });
+
+    res.status(201).cookie("token", token).json({
+      _id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
+  } catch (err) {
+    res.status(400).send("error");
+    console.log(err);
+  }
+};
+
+export const logout = async (req, res) => {
+  res.cookie('token', "", {
+    expires: new Date(0),
+  });
+  return res.sendStatus(204);
 };
